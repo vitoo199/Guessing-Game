cb930125c1085ca52398540f097ca0a0208148f5
vitoo199 <skillowec@gmail.com>
2020-06-28 00:31:34 +0200

    the rest of the program

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -4,9 +4,31 @@ namespace GuessingGame
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+         static void Main(string[] args)
+         {
+             Random rand = new Random();
+             int start = 0;
+             int end = 100;
+             int magicNumber = rand.Next(start, end+1);
+             int guess = -1;
+             Console.WriteLine($"Guess a number between {start} and {end}\nGood Luck!");
+             while (guess != magicNumber)
+             {
+                 try
+                 {
+                      guess = Convert.ToInt16(Console.ReadLine());
+                      Console.WriteLine(
+                          guess > magicNumber ? "Too Large!" : (guess < magicNumber ? "Too small!" : "YOU WON MADAFAKA")
+                          );
+                 }
+                 catch (Exception e)
+                 {
+                     Console.WriteLine("Wrong input!");
+                 }
+             }
+
+         }
+
+       
     }
 }
